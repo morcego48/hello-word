9b31166ad42aaa2f102c501db6dbe8880be0b191
morcego48 <31863011+morcego48@users.noreply.github.com>
2018-07-28 20:51:55 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,30 @@
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <math.h>
+#include <wctype.h>
+#include <wchar.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <locale.h>
+#include <signal.h>
+#include <limits.h>
+#include <float.h>
+#include <iso646.h>
+
+#undef NDEBUG
+#include <assert.h>
+
+int main()
+{
+printf("Hello World\n")
+
+return 0;
+}
